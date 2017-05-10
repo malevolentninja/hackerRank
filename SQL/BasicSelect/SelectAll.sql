2a038d042732bc95ee2218a09d441725894996e1
Olivia Boertje <malevolentninja@gmail.com>
2017-05-10 12:59:37 +0100

    Update SelectAll.sql

--- SQL/BasicSelect/SelectAll.sql
+++ SQL/BasicSelect/SelectAll.sql
@@ -1,1 +1,20 @@
+
+/*
+Question: Query all columns(attributes) for every row in the CITY table. 
+
+Input Format 
+The CITY table is described as folllows: 
+
+          CITY
+-------------------------------
+ID            |   Number
+NAME          |   VARCHAR2(17)
+COUNTRYCODE   |   VARCHAR(3)
+DISTRICT      |   VARCHAR(20)
+POPULATION    |   NUMBER
+
+*/
+
+
+
 SELECT * FROM CITY
